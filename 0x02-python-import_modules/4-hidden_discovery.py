245953fdd8c0ac9de3a8b4188755ff3683c397d4
sundayirvine-code <amugumbiirvine@gmail.com>
2022-12-01 09:49:41 -0800

    hidden_4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4, re
+    names = dir(hidden_4)
+    for name in names:
+        x = re.findall("\A__", name)
+        if x == None:
+            print("{}".format(name))
