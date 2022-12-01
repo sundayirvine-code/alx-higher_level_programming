84b2434b02817f8f3207677822d42ca0bcb9f758
Irvine Sunday <amugumbiirvine@gmail.com>
2022-12-01 21:09:04 +0300

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def inf_add():
     import sys
-    l=len(sys.argv) - 1
+    l = len(sys.argv) - 1
     if l == 0:
         print("{}".format(0))
     else:
-        add=0
-        for i in range(l+1):
+        add = 0
+        for i in range(l + 1):
             if i != 0:
                 add += int(sys.argv[i])
         print("{}".format(add))
