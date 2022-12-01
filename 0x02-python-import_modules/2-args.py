8531d215b864bcb981a62689338594ec29653793
sundayirvine-code <amugumbiirvine@gmail.com>
2022-12-01 09:11:28 -0800

    making a dynamic script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    l = len(sys.argv) - 1
+    if l > 0:
+        if l == 1:
+            print("{} argument:".format(l))
+        else:
+            print("{} arguments:".format(l))
+    else:
+        print("{} arguments.".format(l))
+    for i in range(1, l + 1):
+        print("{}: {}".format(i, sys.argv[i]))
