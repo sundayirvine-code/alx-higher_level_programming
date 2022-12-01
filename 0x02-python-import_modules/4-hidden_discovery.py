58a13cd32db900baa2c8c4eb138dd383f0e671f5
Irvine Sunday <amugumbiirvine@gmail.com>
2022-12-01 20:54:16 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import hidden_4, re
-names = dir(hidden_4)
-for name in names:
-    x = re.findall("\A__", name)
-    if x == None:
-        print("{}".format(name))
+if __name__ == "__main__":
+    import hidden_4, re
+    names = dir(hidden_4)
+    for name in names:
+        x=re.findall("\A__", name)
+        if x:
+        else:
+            print("{}".format(name))
