3927a9ababa7027c7919bb37fd24a2879ca20230
sundayirvine-code <amugumbiirvine@gmail.com>
2022-12-03 01:36:27 -0800

    remove c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 def no_c(my_string):
     s = ''
     for i in my_string:
-        if i == 'c' or i == 'C':
+        if i is 'c' or i is 'C':
             continue
         else:
             s += i
-    return s
+    return (s)
