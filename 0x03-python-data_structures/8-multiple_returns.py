109d80a49718e1015479198407d931349cd4c000
sundayirvine-code <amugumbiirvine@gmail.com>
2022-12-03 02:46:35 -0800

    handle none

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     l = len(sentence)
-    f = sentence[0]
+    if l == 0:
+        f = None
+    else:
+        f = sentence[0]
     return (l, f)
