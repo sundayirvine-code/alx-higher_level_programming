c721140b3922d95dd1312e308c907bde8ef24267
sundayirvine-code <amugumbiirvine@gmail.com>
2022-12-03 01:38:34 -0800

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,6 @@
 def no_c(my_string):
     s = ''
     for i in my_string:
-        if i is 'c' or i is 'C':
-            continue
-        else:
+        if i != 'c' and i != 'C':
             s += i
     return (s)
