e397765694570abf1279fdeb17d74a6a9fd85810
sundayirvine-code <amugumbiirvine@gmail.com>
2022-12-03 01:14:06 -0800

    when empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
     l = len(my_list) - 1
     while(l >= 0):
         print("{:d}".format(my_list[l]))
