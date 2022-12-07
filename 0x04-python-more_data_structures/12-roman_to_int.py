122661d9e58ade28695eb3af1fa94337b9ec6f2c
sundayirvine-code <amugumbiirvine@gmail.com>
2022-12-07 12:11:12 -0800

    roman numbes

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    l = len(roman_string)
+    numeral = 1
+    for i in range(l):
+        try:
+            curr = roman_string[i]
+            nex = roman_string[i + 1]
+            if curr == 'I' and nex == 'V':
+                numeral += 4
+                i += 2
+            elif curr == 'I' and nex == 'X':
+                numeral += 9
+                i += 2
+            elif curr == 'X' and nex == 'L':
+                numeral += 40
+                i += 2
+            elif curr == 'X' and nex == 'C':
+                numeral += 90
+                i += 2
+            elif curr == 'C' and nex == 'D':
+                numeral += 400
+                i += 2
+            elif curr == 'C' and nex == 'M':
+                numeral += 900
+                i += 2
+            else:
+                if curr == 'I':
+                    numeral += 1
+                    i += 1
+                elif curr == 'V':
+                    numeral += 5
+                    i += 1
+                elif curr == 'X':
+                    numeral += 10
+                    i += 1
+                elif curr == 'L':
+                    numeral += 50
+                    i += 1
+                elif curr == 'C':
+                    numeral += 100
+                    i += 1
+                elif curr == 'D':
+                    numeral += 500
+                    i += 1
+                elif curr == 'M':
+                    numeral += 1000
+                    i += 1
+        except IndexError:
+            return numeral
