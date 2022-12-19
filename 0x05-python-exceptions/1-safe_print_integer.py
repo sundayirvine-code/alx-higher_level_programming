0358e5f9c820e2b7388af8965984a21e0bdf188b
sundayirvine-code <amugumbiirvine@gmail.com>
2022-12-19 00:27:35 -0800

    initial commit

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        value = int(value)
+    except ValueError:
+        return False
+    print("{:d}".format(value))
+    return True
