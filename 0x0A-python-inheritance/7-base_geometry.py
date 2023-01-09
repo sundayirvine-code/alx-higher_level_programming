01357e6df264fd213f7970445c73c427fe365602
sundayirvine-code <amugumbiirvine@gmail.com>
2023-01-09 03:29:13 -0800

    7-base_geometry.py, tests/7-base_geometry.txt

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -18,10 +18,11 @@ class BaseGeometry:
         Args:
             name: a string
             value: an integer
-        Raises: TypeError if value is not an iteger 
-        and valueError if value is less than 0.
+        Raises:
+            TypeError: if value is not an iteger 
+            ValueError: if value is less than 0.
         '''
         if type(value) != int:
-            raise TypeError('<name> must be an integer')
+            raise TypeError('{} must be an integer'.format(self.name))
         if value < 0:
-            raise ValueError('<name> must be greater than 0')
+            raise ValueError('{} must be greater than 0'.format(self.name))
