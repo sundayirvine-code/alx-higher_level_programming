fdfe8c9ca91cc18a556a5269a196e4f7d981a94c
Irvine Sunday <amugumbiirvine@gmail.com>
2023-01-09 14:13:28 +0300

    Update 5-base_geometry.py

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-''' Module: 5-base_geometry
+'''
+An empty BaseGeometry class.
 '''
 class BaseGeometry:
-    """An empty class"""
+    """class: BaseGeometry.
+    """
     pass
