761fb710c78bb67b495542f1ea3706c65b2fcfe9
sundayirvine-code <amugumbiirvine@gmail.com>
2023-01-10 05:46:56 -0800

    enable reading of a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Module:0-read_file'''
+def read_file(filename=""):
+    '''Reads a text file and prints contents
+    Args:
+        filename: path to file to be read
+    '''
+    with open(filename, encoding='UTF-8') as f:
+        data = f.read()
+        print(data)
