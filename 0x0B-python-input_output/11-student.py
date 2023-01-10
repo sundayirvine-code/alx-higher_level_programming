c1c76cb3fcd5fe55aeb13be20257912d3f25cee0
Irvine Sunday <amugumbiirvine@gmail.com>
2023-01-10 20:01:21 +0300

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -30,10 +30,10 @@ class Student:
         else:
             return {k: v for k, v in self.__dict__.items() if k in attrs}
 
-        def reload_from_json(self, json):
-        """Replace all attributes of the Student.
-        Args:
-            json (dict): The key/value pairs to replace attributes with.
-        """
-        for k, v in json.items():
-            setattr(self, k, v)
+    def reload_from_json(self, json):
+    """Replace all attributes of the Student.
+    Args:
+        json (dict): The key/value pairs to replace attributes with.
+    """
+    for k, v in json.items():
+        setattr(self, k, v)
