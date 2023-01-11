8b69a5da2e320116cd0392ea6450e34b1add5999
Irvine Sunday <amugumbiirvine@gmail.com>
2023-01-11 04:38:48 +0300

    Update 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,15 +1,9 @@
 #!/usr/bib/python3
 def append_after(filename="", search_string="", new_string=""):
-    content = ''
-
-    with open(filename, 'r', encoding='utf-8') as f:
-        for line in f.readlines():
-            #if line contains search string
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.write(line)
             if search_string in line:
-                content += line
-                content += new_string
-            else:
-                content += line
-                
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write(content)
+                file.write(new_string)
