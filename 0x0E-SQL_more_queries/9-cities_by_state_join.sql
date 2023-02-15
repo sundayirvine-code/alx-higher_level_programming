007311dbdccc228ea34434ec496488887c331211
Irvine Sunday <amugumbiirvine@gmail.com>
2023-02-15 17:19:52 +0300

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,6 +5,6 @@
 
 SELECT cities.id, cities.name, states.name
 FROM cities
-FULL OUTER JOIN states
-ON cities.states_id = states.id
+INNER JOIN states
+ON cities.state_id = states.id
 ORDER BY cities.id ASC;
