e24125998f8baee093f39f99443b0cc6ea6c734d
Irvine Sunday <amugumbiirvine@gmail.com>
2023-02-15 17:01:15 +0300

    Update 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,6 +2,6 @@
 -- Results must be sorted in ascending order by cities.id
 -- You are not allowed to use the JOIN keyword
 
-SELECT id, name FROM CITIES WHERE state_id IN (
+SELECT id, name FROM cities WHERE state_id IN (
   SELECT state_id FROM states WHERE name = 'California')
   
