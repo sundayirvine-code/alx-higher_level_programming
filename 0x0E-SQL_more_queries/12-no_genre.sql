ac5e046a8f608cdf772c7a29e56d28ca9d04f2e4
sundayirvine-code <amugumbiirvine@gmail.com>
2023-02-15 07:05:44 -0800

    created 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -5,6 +5,7 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT OUTER JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
