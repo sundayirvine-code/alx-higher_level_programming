3001b064d597bbbce832edb206a252f5258f7f58
sundayirvine-code <amugumbiirvine@gmail.com>
2023-02-15 05:29:26 -0800

    3-force_name.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 --  creates the table force_name on your MySQL server.
 -- If the table force_name already exists, your script should not fail
 
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
 
