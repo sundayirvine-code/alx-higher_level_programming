5ddcb426799df71cb18a22fd7007c08185eb1eed
Irvine Sunday <amugumbiirvine@gmail.com>
2023-02-15 17:10:31 +0300

    Create 9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,10 @@
+-- lists all cities contained in the database hbtn_0d_usa.
+-- Each record should display: cities.id - cities.name - states.name
+-- Results must be sorted in ascending order by cities.id
+-- You can use only one SELECT statement
+
+USE hbtn_0d_usa;
+SELECT cities.id, cities.name, states.name
+FROM cities
+FULL OUTER JOIN states
+ORDER BY cities.id
