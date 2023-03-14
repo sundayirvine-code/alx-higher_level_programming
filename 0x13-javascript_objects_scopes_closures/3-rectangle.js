9baa5ab6f5cb0420ead2092607fbacdd607f11d4
sundayirvine-code <amugumbiirvine@gmail.com>
2023-03-14 12:16:42 +0300

    created 4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(w, h) {
+      if (w <= 0 || h <= 0) {
+        // Create empty object if either argument is not positive
+        return {};
+      }
+  
+      // Otherwise, initialize instance attributes
+      this.width = w;
+      this.height = h;
+    }
+  
+    print() {
+      let row = 'X'.repeat(this.width);
+  
+      for (let i = 0; i < this.height; i++) {
+        console.log(row);
+      }
+    }
+  }
+  
