351cd920c6d5206f846ef77d5216d51263bba1e1
sundayirvine-code <amugumbiirvine@gmail.com>
2023-03-14 12:25:04 +0300

    created 8-esrever.js

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+    let reversed = [];
+    for (let i = list.length - 1; i >= 0; i--) {
+      reversed.push(list[i]);
+    }
+    return reversed;
+  };
+  
