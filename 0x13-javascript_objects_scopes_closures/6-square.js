002b4a710afbc0934510d8c860b01051f3e96ae9
sundayirvine-code <amugumbiirvine@gmail.com>
2023-03-14 12:18:53 +0300

    created 6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const Square = require('./5-square');
+
+class SquareWithCharPrint extends Square {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = SquareWithCharPrint;
