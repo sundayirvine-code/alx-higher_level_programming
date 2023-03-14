e2061aa95c938095785e823aff980dcf2144b21f
Irvine Sunday <amugumbiirvine@gmail.com>
2023-03-14 19:30:07 +0300

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
+
 class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0) {
-        // Create empty object if either argument is not positive
-        return {};
-      }
-  
-      // Otherwise, initialize instance attributes
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
+}
+
+module.exports = Rectangle;
   
