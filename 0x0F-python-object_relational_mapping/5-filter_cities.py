95c032ad4c311a1a3b6e23a4cbf47bec8d085bf1
Irvine Sunday <amugumbiirvine@gmail.com>
2023-03-19 13:47:24 +0300

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python3
 """
-lists all cities from the database
+python script that lists all cities from the database hbtn_0e_4_usa
+with specified state name
 """
+
+import MySQLdb
+from sys import argv
+
 if __name__ == "__main__":
-
-    import MySQLdb
-    from sys import argv
-
-    cont = 0
-    conect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                             passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = conect.cursor()
-    cursor.execute("""SELECT cities.id, cities.name, states.name
-    FROM cities
-    LEFT JOIN states ON cities.state_id = states.id
-    ORDER BY cities.id ASC""")
-    query_rows = cursor.fetchall()
-    for row in query_rows:
-        if row[2] == argv[4]:
-            if cont > 0:
-                print(", ", end="")
-            print(row[1], end="")
-            cont = cont + 1
-    print()
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
+    ORDER BY cities.id", (argv[4],))
+    rows = cursor.fetchall()
+    print(", ".join(city[0] for city in rows))
     cursor.close()
-    conect.close()
+    db.close()
