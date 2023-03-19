4a69c8daab2a3d94ba3f36ac2add80db97dd0c7b
Irvine Sunday <amugumbiirvine@gmail.com>
2023-03-19 13:37:16 +0300

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-'''
-a script that lists all State objects
-from the database hbtn_0e_6_usa
-'''
+"""
+prints the State object with the name passed as argument from a database
+"""
 
-
-from sys import argv
+import sqlalchemy
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                        argv[2],
-                                                        argv[3]))
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
     state = session.query(State).filter_by(name=argv[4]).first()
     if state is not None:
-            print(str(state.id))
+        print(str(state.id))
     else:
         print("Not found")
     session.close()
