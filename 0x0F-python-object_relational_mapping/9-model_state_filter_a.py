30d69d571775bb9aa9036843d93adbc10122a194
Irvine Sunday <amugumbiirvine@gmail.com>
2023-03-19 13:51:06 +0300

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """
-All states via SQLAlchemy
+list all State objects that contain the letter a from a database
 """
+
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    data = session.query(State).order_by(State.id)\
-                               .filter(State.name.like("%a%")).all()
-    for row in data:
-        print("{}: {}".format(row.id, row.name))
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    s = '%a%'
+    states = session.query(State).filter(State.name.like(s)).order_by(State.id)
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
     session.close()
