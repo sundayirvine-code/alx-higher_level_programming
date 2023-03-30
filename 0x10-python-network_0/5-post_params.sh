bc0442954d31c056b203521146bf830c3a54a45e
Irvine Sunday <amugumbiirvine@gmail.com>
2023-03-30 14:11:40 +0300

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a POST request to the URL with two parameters, and displays the response body.
-
-# Check if a URL is provided
-if [ -z "$1" ]; then
-  echo "Please provide a URL"
-  exit 1
-fi
-
-# Send a POST request with two parameters and display the response body
-curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
-
+# Script sends POST to URL
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
