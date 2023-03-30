12255b127067bbad7f606dfea410058f588a0eff
Irvine Sunday <amugumbiirvine@gmail.com>
2023-03-30 14:07:16 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a request to that URL, and displays the size of the body of the response in bytes.
-
-# Check if a URL is provided
-if [ -z "$1" ]; then
-  echo "Please provide a URL"
-  exit 1
-fi
-
-# Send request and display the size of the body
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
-
+# Script that takes in a URL, sends a request and displays the size of the body of the response
+curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
