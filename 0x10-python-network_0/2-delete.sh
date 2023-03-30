30f56eaec2b6f51dca29028c531d34648b3000f7
Irvine Sunday <amugumbiirvine@gmail.com>
2023-03-30 14:09:06 +0300

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# This script sends a DELETE request to the URL passed as the first argument and displays the body of the response.
-
-# Check if a URL is provided
-if [ -z "$1" ]; then
-  echo "Please provide a URL"
-  exit 1
-fi
-
-# Send DELETE request and display the body of the response
-curl -sX DELETE "$1"
-
+# Script sends a DELETE request to URL passed as the first argument
+curl -sX "DELETE" "$1"
