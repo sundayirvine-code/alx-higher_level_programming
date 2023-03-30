72a8078f75db49178611712671af8b59e8f059ca
Irvine Sunday <amugumbiirvine@gmail.com>
2023-03-30 14:10:01 +0300

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument and displays all HTTP methods the server will accept.
-
-# Check if a URL is provided
-if [ -z "$1" ]; then
-  echo "Please provide a URL"
-  exit 1
-fi
-
-# Send an OPTIONS request and display the Allow header
+# Script that takes in a URL and displays all HTTP methods the server will accept
 curl -sI "$1" | grep "Allow" | cut -d " " -f2-
-
