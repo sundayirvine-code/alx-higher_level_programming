13b9f99dd2bbd47bde845288019113235c51a31c
Irvine Sunday <amugumbiirvine@gmail.com>
2023-03-30 14:10:46 +0300

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 # This script takes a URL as an argument, sends a GET request to the URL with a custom header, and displays the response body.
-
-# Check if a URL is provided
-if [ -z "$1" ]; then
-  echo "Please provide a URL"
-  exit 1
-fi
-
-# Send a GET request with a custom header and display the response body
 curl -sH "X-School-User-Id: 98" "$1"
 
